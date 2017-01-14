3038b768dcf954f023840f2a077434a5164329d2
Tiberiu Ichim <tibi@pixelblaster.ro>
2017-01-14 08:19:57 +0200

    Updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,5 @@
 type virtualenv >/dev/null 2>&1 || { echo >&2 "Needs virtualenv in PATH."; exit 1; }
 virtualenv .
 bin/pip install -r requirements.txt
+curl https://github.com/spf13/hugo/releases/download/v0.18.1/hugo_0.18.1_Linux-64bit.tar.gz > /tmp/hugo.tgz
+tar xzf /tmp/hugo.tgz
---
+++ content/post/good-to-read.md
@@ -0,0 +1,7 @@
++++
+draft = true
+date = "2017-01-12T22:05:51+02:00"
+title = "good to read"
+
++++
+
