ecd3242dd152acff4eb2385523e3857d2bfb62fa
Tiberiu Ichim <tibi@pixelblaster.ro>
2017-04-23 15:59:15 +0300

    Use hugo 0.20 to generate files

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-type virtualenv >/dev/null 2>&1 || { echo >&2 "Needs virtualenv in PATH."; exit 1; }
-virtualenv .
-bin/pip install -r requirements.txt
-curl https://github.com/spf13/hugo/releases/download/v0.18.1/hugo_0.18.1_Linux-64bit.tar.gz > /tmp/hugo.tgz
+HUGO="https://github.com/spf13/hugo/releases/download/v0.20.2/hugo_0.20.2_Linux-64bit.tar.gz"
+curl -L $HUGO > /tmp/hugo.tgz
 tar xzf /tmp/hugo.tgz
