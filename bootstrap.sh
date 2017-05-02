a5086c5d0899e1360a74fdbb3e925710332235e8
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2017-05-02 10:16:20 +0300

    Regen files with proper rss feed location

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,5 @@
 HUGO="https://github.com/spf13/hugo/releases/download/v0.20.2/hugo_0.20.2_Linux-64bit.tar.gz"
 curl -L $HUGO > /tmp/hugo.tgz
 tar xzf /tmp/hugo.tgz
+mkdir -p bin/
+mv hugo_0.20.2_linux_amd64/hugo_0.20.2_linux_amd64 bin/hugo
